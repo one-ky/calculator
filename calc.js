1797a1d06f4af7f1441f5faca682b4156a2c573c
Kyle ONeill <kylepatrick142@gmail.com>
2022-06-24 21:46:31 -0400

    update html with more concise classes, add functions to js file

--- calc.js
+++ calc.js
@@ -1,33 +1,58 @@
-function add (a,b) {
-    let sum = (a+b).toFixed(2)
-    return sum
-}
-function subtract (a,b) {
-    let diff = (a-b).toFixed(2)
-    return diff
-}
-function multiply (a,b) {
-    let product = (a*b).toFixed(2)
-    return product
-}
-function divide(a,b) {
-    let quotient = (a/b).toFixed(2)
-    return quotient
-}
-function operate (operator, a, b) {
-    return operator(a,b)
+function operate (num1, num2, operator) {
+    let value
+    switch(operator) {
+        case "+":
+            value = parseFloat(num1) + parseFloat(num2)
+            break
+        case "-":
+            value = parseFloat(num1) - parseFloat(num2)
+            break
+            
+        case "*":
+            value = parseFloat(num1) - parseFloat(num2)
+            break
+
+        case "/":
+            value = parseFloat(num1) - parseFloat(num2)
+            break  
+    }
+    return value
 }
 
-console.log(operate(divide, 5, 5))
 
-// function makeNumberPad () {
-//     let pad = document.querySelector(".number-pad")
-//     let size = 9
-//     for (let i=0; i<=size; i++){
-//         let number = document.createElement('div')
-//         number.innerText = i
-//         numberPad.appendChild(number).className = 'number-item'
-//     }
-// }
-// makeNumberPad()
+function clearAll(){
+
+}
+
+function deleteLast(){
+
+}
+
+function equals(){
+
+}
+
+function updateScreen(currentInput){
+        
+
+}
+
+const digitButton = document.querySelectorAll('.digit')
+const operatorButton = document.querySelectorAll('.operator')
+const equalsButton = document.querySelector('.equalsOperator')
+const delButton = document.querySelector('.delOperator')
+const clearButton = document.querySelector('.clearOperator')
+
+
+digitButton.forEach(button => {
+    button.addEventListener('click', () => {
+
+    })
+})
+
+operatorButton.forEach(button => {
+    button.addEventListener('click', () => {
+
+    })
+})
 
